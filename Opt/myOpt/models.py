839bda5751fb5b3e5a03888dde74b1ec67c0627d
kulmehmet <2kulmehmet@gmail.com>
2017-05-23 22:16:04 +0300

    initialization

---
+++ Opt/myOpt/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.template.defaultfilters import default
+from datetime import date
+from unittest.util import _MAX_LENGTH
+
+# Create your models here.
+
+class OptimizationData(models.Model):
+    date = models.DateField();
+    store = models.CharField(max_length = 50)
+    location = models.CharField(max_length = 100)
+    code = models.IntegerField()
+    salerProductName = models.CharField(max_length = 50)
+    mainGroup = models.CharField(max_length = 50)
+    subGroup = models.CharField(max_length = 50)
+    productVariety = models.CharField(max_length = 50)
+    salesAmount = models.IntegerField()
+    
+    
+    
---
+++ Opt/myOpt/views.py
@@ -0,0 +1,83 @@
+from django.shortcuts import render
+import csv
+from myOpt.models import OptimizationData
+from datetime import datetime
+import datetime
+from django.http import HttpResponse
+import xlsxwriter
+
+
+#Create your views here.
+def readAndWriteData(request):
+    OptimizationData.objects.all().delete()
+    counter = 0
+    csv_file = open('example.csv')
+    csv_reader = csv.reader(csv_file, delimiter = ',')
+    next(csv_reader)
+    for row in csv_reader:
+       # d = datetime.date(row[0])
+        s = row[0]
+        d =datetime.datetime.strptime(s, '%d-%m-%Y').strftime('%Y-%m-%d')
+       # counter = counter +1
+    #print(counter)
+        variables = OptimizationData(date = d, store = row[1],location = row[2],code = row[3],salerProductName = row[4],mainGroup = row[5],subGroup = row[6],productVariety = row[7],salesAmount = row[8])
+        variables.save();
+    csv_file.close()
+
+    readFromDB()
+    return HttpResponse('finito')
+ 
+def readFromDB():
+     opt = OptimizationData.objects.filter()
+     workbook = xlsxwriter.Workbook('Expenses02.xlsx')
+     worksheet = workbook.add_worksheet('Sheet 1')
+     bold = workbook.add_format({'bold': True})
+     worksheet.write('A1', 'Gun', bold)
+     worksheet.write('B1', 'Magaza', bold)
+     worksheet.write('C1', 'Lokasyon', bold)
+     worksheet.write('D1', 'Kod', bold)
+     worksheet.write('E1', 'Satici/UrunAdi', bold)
+     worksheet.write('F1', 'AnaGrup', bold)
+     worksheet.write('G1', 'AltGrup', bold)
+     worksheet.write('H1', 'UrunCesidi', bold)
+     worksheet.write('I1', 'SatisMiktari', bold)
+# Some data we want to write to the worksheet.
+
+# Start from the first cell. Rows and columns are zero indexed.
+     row = 1
+     col = 0
+
+# Iterate over the data and write it out row by row.
+     for obj in opt:
+        worksheet.write(row, col,str(obj.date))
+        worksheet.write(row, col + 1, obj.store)
+        worksheet.write(row, col + 2, obj.location)
+        worksheet.write(row, col + 3,str( obj.code))
+        worksheet.write(row, col + 4, obj.salerProductName)
+        worksheet.write(row, col + 5, obj.mainGroup)
+        worksheet.write(row, col + 6, obj.subGroup)
+        worksheet.write(row, col + 7, obj.productVariety)
+        worksheet.write(row, col + 8, obj.salesAmount)
+        row += 1
+
+# Write a total using a formula.
+
+     chart = workbook.add_chart({'type': 'line'})
+
+
+# Configure the chart. In simplest case we add one or more data series.
+     chart.add_series({
+         'values': ['Sheet 1', 1, 8,row-1 ,8],
+         'categories' : ['Sheet 1', 1, 0, row-1, 0],
+        'line' : {'color': 'blue'},
+         'name' : 'Real Amount',
+     })
+     chart.set_title ({'name': 'Optimization Results'})
+     chart.set_x_axis({'name': 'Sales Amount'})
+     chart.set_y_axis({'name': 'Date'})
+    
+     worksheet.insert_chart('K1', chart)
+ 
+     workbook.close()
+     
+        
