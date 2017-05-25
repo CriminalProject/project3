8c6560bf2d26e42d3d2a86e4b67962e40339f0cf
kulilber <safa_185@windowslive.com>
2017-05-25 04:37:17 -0700

    eliminate data to process

--- Opt/myOpt/views.py
+++ Opt/myOpt/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 import csv
-from myOpt.models import OptimizationData
-from datetime import datetime
+from myOpt.models import OptimizationData,FinalData
+from datetime import datetime,timedelta
 import datetime
 from django.http import HttpResponse
 import xlsxwriter
@@ -14,33 +14,69 @@ def readAndWriteData(request):
     csv_file = open('example.csv')
     csv_reader = csv.reader(csv_file, delimiter = ',')
     next(csv_reader)
+    objList = []
     for row in csv_reader:
        # d = datetime.date(row[0])
         s = row[0]
         d =datetime.datetime.strptime(s, '%d-%m-%Y').strftime('%Y-%m-%d')
-       # counter = counter +1
-    #print(counter)
-        variables = OptimizationData(date = d, store = row[1],location = row[2],code = row[3],salerProductName = row[4],mainGroup = row[5],subGroup = row[6],productVariety = row[7],salesAmount = row[8])
-        variables.save();
+        
+        newRow = OptimizationData(date = d, store = row[1],location = row[2],code = row[3],salerProductName = row[4],mainGroup = row[5],subGroup = row[6],productVariety = row[7],salesAmount = row[8])
+        objList.append(newRow)
+        counter = counter + 1
+        if counter % 20 == 0:
+            OptimizationData.objects.bulk_create(objList)
+            objList = []
+       
+       
+    if objList:
+         OptimizationData.objects.bulk_create(objList)
+        
     csv_file.close()
-
-    readFromDB()
     return HttpResponse('finito')
  
-def readFromDB():
-     opt = OptimizationData.objects.filter()
-     workbook = xlsxwriter.Workbook('Expenses02.xlsx')
+def readFromDB(request):
+     oldopt = OptimizationData.objects.filter(productVariety = 'cesit-1').order_by('date')
+     FinalData.objects.all().delete()
+     count = 0
+     objlist = []
+     for obj in oldopt:
+         if count == 0:
+             lastDate = obj.date
+             lastAmount = obj.salesAmount
+         else:
+            if lastDate == obj.date:
+                obj.salesAmount = lastAmount + obj.salesAmount
+                lastObj = obj
+            else:
+                newEntry = FinalData(date = lastObj.date,salesAmount = lastObj.salesAmount)
+                objlist.append(newEntry)
+                lastObj = obj
+                lastDate = obj.date
+                
+            if len(objlist) == 20:
+                FinalData.objects.bulk_create(objlist)
+                objlist = []
+         count+= 1   
+    
+     if objlist:
+         FinalData.objects.bulk_create(objlist)
+         
+     opt = FinalData.objects.all()
+     opt2 = FinalData.objects.all()
+     workbook = xlsxwriter.Workbook('Expenses03.xlsx')
      worksheet = workbook.add_worksheet('Sheet 1')
      bold = workbook.add_format({'bold': True})
-     worksheet.write('A1', 'Gun', bold)
-     worksheet.write('B1', 'Magaza', bold)
-     worksheet.write('C1', 'Lokasyon', bold)
-     worksheet.write('D1', 'Kod', bold)
-     worksheet.write('E1', 'Satici/UrunAdi', bold)
-     worksheet.write('F1', 'AnaGrup', bold)
-     worksheet.write('G1', 'AltGrup', bold)
-     worksheet.write('H1', 'UrunCesidi', bold)
-     worksheet.write('I1', 'SatisMiktari', bold)
+     worksheet.write('A1', 'Date', bold)
+     worksheet.write('B1', 'Amount', bold)
+    # worksheet.write('A1', 'Gun', bold)
+    # worksheet.write('B1', 'Magaza', bold)
+    # worksheet.write('C1', 'Lokasyon', bold)
+    # worksheet.write('D1', 'Kod', bold)
+    # worksheet.write('E1', 'Satici/UrunAdi', bold)
+    # worksheet.write('F1', 'AnaGrup', bold)
+    # worksheet.write('G1', 'AltGrup', bold)
+    # worksheet.write('H1', 'UrunCesidi', bold)
+    # worksheet.write('I1', 'SatisMiktari', bold)
 # Some data we want to write to the worksheet.
 
 # Start from the first cell. Rows and columns are zero indexed.
@@ -50,14 +86,14 @@ def readFromDB():
 # Iterate over the data and write it out row by row.
      for obj in opt:
         worksheet.write(row, col,str(obj.date))
-        worksheet.write(row, col + 1, obj.store)
-        worksheet.write(row, col + 2, obj.location)
-        worksheet.write(row, col + 3,str( obj.code))
-        worksheet.write(row, col + 4, obj.salerProductName)
-        worksheet.write(row, col + 5, obj.mainGroup)
-        worksheet.write(row, col + 6, obj.subGroup)
-        worksheet.write(row, col + 7, obj.productVariety)
-        worksheet.write(row, col + 8, obj.salesAmount)
+        worksheet.write(row, col + 1, obj.salesAmount)
+ #      worksheet.write(row, col + 2, obj.location)
+ #      worksheet.write(row, col + 3,str( obj.code))
+ #      worksheet.write(row, col + 4, obj.salerProductName)
+ #      worksheet.write(row, col + 5, obj.mainGroup)
+ #      worksheet.write(row, col + 6, obj.subGroup)
+ #      worksheet.write(row, col + 7, obj.productVariety)
+ #      worksheet.write(row, col + 8, obj.salesAmount)
         row += 1
 
 # Write a total using a formula.
@@ -67,7 +103,7 @@ def readFromDB():
 
 # Configure the chart. In simplest case we add one or more data series.
      chart.add_series({
-         'values': ['Sheet 1', 1, 8,row-1 ,8],
+         'values': ['Sheet 1', 0, 1,row-1 ,1],
          'categories' : ['Sheet 1', 1, 0, row-1, 0],
         'line' : {'color': 'blue'},
          'name' : 'Real Amount',
@@ -79,5 +115,5 @@ def readFromDB():
      worksheet.insert_chart('K1', chart)
  
      workbook.close()
-     
+     return HttpResponse('readFinito')
         
