42a4bd710416fd0376851d02f71a3428e09c9b13
kulmehmet <2kulmehmet@gmail.com>
2017-05-27 16:11:01 +0300

    last changes..

--- Opt/myOpt/views.py
+++ Opt/myOpt/views.py
@@ -7,6 +7,15 @@ from django.http import HttpResponse
 import xlsxwriter
 from numpy.lib.function_base import average
 import math
+from pandas import Series
+from pandas import DataFrame
+from pandas import concat
+from matplotlib import pyplot
+import numpy as np
+from pandas.core.frame import DataFrame
+from sklearn.metrics import mean_squared_error
+from statsmodels.tsa.ar_model import AR
+from statsmodels.tsa.arima_model import ARIMA
 
 #Create your views here.
 def readAndWriteData(request):
@@ -17,7 +26,6 @@ def readAndWriteData(request):
     next(csv_reader)
     objList = []
     for row in csv_reader:
-       # d = datetime.date(row[0])
         s = row[0]
         d =datetime.datetime.strptime(s, '%d-%m-%Y').strftime('%Y-%m-%d')
         
@@ -27,7 +35,6 @@ def readAndWriteData(request):
         if counter % 20 == 0:
             OptimizationData.objects.bulk_create(objList)
             objList = []
-       
        
     if objList:
          OptimizationData.objects.bulk_create(objList)
@@ -188,7 +195,7 @@ def readFromDB(request):
          temp = math.ceil(temp)
          predictionArray.append(temp)
          predictCount+=1      
-     workbook = xlsxwriter.Workbook('Expenses055.xlsx')
+     workbook = xlsxwriter.Workbook('Prediction.xlsx')
      worksheet = workbook.add_worksheet('Sheet 1')
      bold = workbook.add_format({'bold': True})
      worksheet.write('A1', 'Date', bold)
@@ -197,22 +204,20 @@ def readFromDB(request):
      worksheet.write('D1', 'RoundFit', bold)
      worksheet.write('E1', 'RealFeb', bold)
      worksheet.write('F1', 'PredictedFeb', bold)
-    # worksheet.write('A1', 'Gun', bold)
-    # worksheet.write('B1', 'Magaza', bold)
-    # worksheet.write('C1', 'Lokasyon', bold)
-    # worksheet.write('D1', 'Kod', bold)
-    # worksheet.write('E1', 'Satici/UrunAdi', bold)
-    # worksheet.write('F1', 'AnaGrup', bold)
-    # worksheet.write('G1', 'AltGrup', bold)
-    # worksheet.write('H1', 'UrunCesidi', bold)
-    # worksheet.write('I1', 'SatisMiktari', bold)
-# Some data we want to write to the worksheet.
+     worksheet.write('H1','Date',bold)
+     worksheet.write('I1','Predicted Amount',bold)
+     worksheet.write('J1','Real Amount',bold)
 
-# Start from the first cell. Rows and columns are zero indexed.
      row = 1
      col = 0
+     row2 = 1
+     col2 = 7
+     row3 =1
+     array = []
+     date  = []
+     sayac = 0
+     
 
-# Iterate over the data and write it out row by row.
      totalCount = 0
      for obj in opt:
         worksheet.write(row, col,str(obj.date))
@@ -222,22 +227,54 @@ def readFromDB(request):
         if totalCount<28:
             worksheet.write(row, col + 4, realArray[totalCount].salesAmount)
             worksheet.write(row, col + 5, predictionArray[totalCount])
- #      worksheet.write(row, col + 2, obj.location)
- #      worksheet.write(row, col + 3,str( obj.code))
- #      worksheet.write(row, col + 4, obj.salerProductName)
- #      worksheet.write(row, col + 5, obj.mainGroup)
- #      worksheet.write(row, col + 6, obj.subGroup)
- #      worksheet.write(row, col + 7, obj.productVariety)
- #      worksheet.write(row, col + 8, obj.salesAmount)
+        if sayac>=259 and sayac <= 286:
+            worksheet.write(row3,col2,str(obj.date))
+            row3+=1
+        array.append(obj.salesAmount)
+        date.append(str(obj.date))
         row += 1
+        sayac+=1
         if totalCount!=333:
             totalCount+=1
 # Write a total using a formula.
+     X = array
+     size =int( len(X) * 0.916)
+     train, test, tasi= X[0:259], X[259:287],X[287:len(X)]
+     train.extend(tasi)
+     print ('TEST')
+     print(test)
+     print(train)
+     history = [x for x in train]
+     a =np.array(history, dtype=np.float64)
+     p = a.tolist()
+     myarr = []
+     myarr = p
+     sayac2 = 0
+     predictions = list()
+     for t in range(len(test)):
+         sayac2+=1     
+         model = ARIMA(myarr[0:len(train)-len(test)+t], order=(5,1,0))
+         model_fit = model.fit(disp=False)
+         output = model_fit.forecast()
+         yhat = output[0]
+         predictions.append(yhat)
+         obs = test[t]
+         history.append(obs)
+         worksheet.write(row2,col2+1,yhat)
+         worksheet.write(row2,col2+2,obs)
+         row2+=1
+     error = mean_squared_error(test, predictions)
+     print('Test MSE: %.3f' % error)
+
+     pyplot.plot(test)
+     pyplot.plot(predictions, color='red')
+     pyplot.show()
+
+
 
      chart = workbook.add_chart({'type': 'line'})
 
 
-# Configure the chart. In simplest case we add one or more data series.
      chart.add_series({
          'values': ['Sheet 1', 0, 1,row-1 ,1],
          'categories' : ['Sheet 1', 1, 0, row-1, 0],
@@ -290,13 +327,44 @@ def readFromDB(request):
         'line' : {'color': 'orange'},
          'name' : 'roundFit',
      })
-     chart3.set_title ({'name': 'Optimization Results2'})
+     
+     chart4 = workbook.add_chart({'type': 'line'})
+
+     chart4.add_series({ #real
+        'values': ['Sheet 1', 1, 9,row2-1 ,9],
+        'categories' : ['Sheet 1', 1, 7, row3-1, 7],
+        'line' : {'color': 'blue'},
+        'name' : 'Real Amount',
+     })
+     
+     chart4.add_series({ #predicted
+        'values': ['Sheet 1', 1, 8,row2-1 ,8],
+        'categories' : ['Sheet 1', 1, 7, row3-1, 7],
+        'line' : {'color': 'red'},
+        'name' : 'Real Amount',
+     }) 
+     
+     chart.set_title ({'name': 'Weekly Fit'})
+     chart.set_x_axis({'name': 'Sales Amount'})
+     chart.set_y_axis({'name': 'Date'})        
+     
+     chart2.set_title ({'name': '3 Days Fit'})
+     chart2.set_x_axis({'name': 'Sales Amount'})
+     chart2.set_y_axis({'name': 'Date'})   
+     
+     chart3.set_title ({'name': 'Optimization Results with our algorithm (For February)'})
      chart3.set_x_axis({'name': 'Sales Amount'})
      chart3.set_y_axis({'name': 'Date'})
-     worksheet.insert_chart('K1', chart)
-     worksheet.insert_chart('K2', chart2)
-     worksheet.insert_chart('K3', chart3)
- 
+     
+     chart4.set_title ({'name': 'Regression Result with ARIMA  (For February)'})
+     chart4.set_x_axis({'name': 'Sales Amount'})
+     chart4.set_y_axis({'name': 'Date'})
+     
+     worksheet.insert_chart('L1', chart)
+     worksheet.insert_chart('L16', chart2)
+     worksheet.insert_chart('L30', chart3)
+     worksheet.insert_chart('L45', chart4)
+
      workbook.close()
      return HttpResponse('readFinito')
         
